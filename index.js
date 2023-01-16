752441cf6f5c001ce20d5c5b30654e68bc868eb9
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-01-16 21:04:47 +0100

    ADD deployment config

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
-
+const users = require('./db/users.json');
 const PORT = 3000;
 
+// Template engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.json()) // habitlitar el tipo a recibir en el server 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(PORT, () => {
-  console.info(`> Estoy arribísima en el puerto ${PORT}! ✨🦄`);
+  console.info(`> Estoy arribísima en el puerto http://localhost:${PORT} ! ✨🦄`);
 });
+
